3c0a6fcf8a964582db3073afe0d922d7b8737ad4
CodeByNux <Jeff.Nuxoll@gmail.com>
2025-08-07 20:05:35 -0700

    Ignore _temp.py & cleaned up tracked files

--- tests/_temp.py
+++
@@ -1,9 +0,0 @@
-
-from netkit_cisco.device import CiscoDevice
-
-node = CiscoDevice("test","192.168.2.136","admin","MyPassword")
-
-if node.ssh_connect() == False:
-    print("something went wrong")
-else:
-    print("Yay")
